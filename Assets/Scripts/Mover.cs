e9af81d53f2e9f60234c839f35e96693eaf4e27f
Mohab Rabea <mohaprabea@gmail.com>
2025-08-01 04:07:06 +0300

    Intro to VARs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    public float Xvalue = 0.0f;
+    public float Yvalue = 1.0f;
+    public float Zvalue = 0.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = new Vector3(0, 1, 0);
+        transform.position = new Vector3(Xvalue, Yvalue, Zvalue);
     }
 
     // Update is called once per frame
