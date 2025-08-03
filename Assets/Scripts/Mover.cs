793365ac018a2f42288f010475e86f2a28a2bbed
Mohab Rabea <mohaprabea@gmail.com>
2025-08-03 14:00:45 +0300

    Using Input Axis

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float Xvalue = 0.0f;
-    [SerializeField] float Yvalue = 1.0f;
-    [SerializeField] float Zvalue = 0.0f;
+
+    [SerializeField] float speed = 5.0f ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = new Vector3(Xvalue, Yvalue, Zvalue);
+        transform.position = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Xvalue * Time.deltaTime, Yvalue * Time.deltaTime, Zvalue * Time.deltaTime);
+        float Xvalue = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float Yvalue = 0;
+        float Zvalue = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(Xvalue, Yvalue , Zvalue);
     }
 }
