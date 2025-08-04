f1439775fce732b4624c904764df835475def8ac
Mohab Rabea <mohaprabea@gmail.com>
2025-08-04 12:59:08 +0300

    Intro to CineMachine Camera

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,7 +7,7 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = new Vector3(0, 1, 0);
+        
     }
 
     // Update is called once per frame
